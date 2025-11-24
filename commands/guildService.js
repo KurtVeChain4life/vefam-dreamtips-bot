939fd91baaeed6fddca86a78b22df3e45b5de2f8
KurtVeChain4life <vewear2023@gmail.com>
2025-11-24 20:00:02 +0100

    Delete commands/guildService.js

--- commands/guildService.js
+++
@@ -1,8 +0,0 @@
-export async function getGuildConfig(guildId) {
-  const { rows } = await pool.query('SELECT * FROM guild_configs WHERE guild_id = $1', [guildId]);
-  return rows[0]?.data || { activity_token: 'BOOBS', reward_criteria: { characters_per_boobs: 3, roles: [] }, mint_collection: '0x...' };
-}
-
-export async function updateGuildConfig(guildId, config) {
-  await pool.query('INSERT INTO guild_configs (guild_id, data) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET data = $2', [guildId, config]);
-}
